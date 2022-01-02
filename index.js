e2da62938c8994c2cae537b3cd43917cdc3cbd7f
cyrielle istin <cycycode@gmail.com>
2022-01-02 18:46:42 +0100

    initialization of the app with Express and mongoose, adding dotenv :heavy_plus_sign: and connecting the MongoDB database with success :tada: also add the author to package.json

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+// Initialization of the app :
+//  console.log("hello world")
+
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Initialization of Express
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// const router = express.Router()
+
+// Port
+const port = process.env.PORT
+
+// Connexion to MongoDB database 
+const mongoDbConnectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+// Lauch of the connexion with params
+mongoose.connect(mongoDbConnectionString, null, error => {
+  if (error) throw new Error(error)
+})
+
+// Recovery of the connexion
+const db = mongoose.connection
+
+// Listener of the connexion
+db.once('open', () => {
+  console.info('Connexion à la base : OK')
+})
